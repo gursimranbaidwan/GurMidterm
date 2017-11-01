6b4dbbe75665cbd6cb7b4cc9d5190479bda80eac
Student <student@CVS-R213D012.nic.bc.ca>
2017-11-01 12:23:01 -0700

    copyrightsymboldone

--- wp-content/themes/twentysixteen-child/footer.php
+++ wp-content/themes/twentysixteen-child/footer.php
@@ -11,6 +11,7 @@
 ?>
 
 		</div><!-- .site-content -->
+<?php echo comicpress_copyright(); ?>
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<?php if ( has_nav_menu( 'primary' ) ) : ?>
@@ -38,22 +39,6 @@
 				</nav><!-- .social-navigation -->
 			<?php endif; ?>
 
-			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the twentysixteen footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'twentysixteen_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
-		</footer><!-- .site-footer -->
-	</div><!-- .site-inner -->
-</div><!-- .site -->
-
-<?php wp_footer(); ?>
+			
 </body>
 </html>
